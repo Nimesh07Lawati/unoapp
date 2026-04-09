bd9b7569b62065694bf64e228d4057f09ad73143
Nimesh07Lawati <160296238+Nimesh07Lawati@users.noreply.github.com>
2026-04-09 13:43:57 +0545

    main shell bug fixed

--- lib/application_shell/main_shell.dart
+++ lib/application_shell/main_shell.dart
@@ -30,7 +30,7 @@ class MainShell extends StatelessWidget {
     final index = _selectedIndex(location);
 
     return Scaffold(
-      body: child,
+      body: SafeArea(top: true, bottom: false, child: child),
       extendBody: true,
       bottomNavigationBar: Column(
         mainAxisSize: MainAxisSize.min,
